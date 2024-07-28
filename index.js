c56e5fe3b116ba30926bf77389ba8b41b5673e6b
miru6141 <miru6141@gmail.com>
2024-07-28 22:34:57 +0530

    update2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ mongoose.connect(DB_URL);
 
 
 app.use(cors({
-    origin: 'https://referralsystem13.netlify.app/',
+    origin: 'https://referralsystem13.netlify.app',
     credentials: true
 }));
 
